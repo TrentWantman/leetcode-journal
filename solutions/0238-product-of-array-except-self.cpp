348e3940cd3077390c95d21a6125329fc9b53e31
Trent Wantman <93285436+TrentWantman@users.noreply.github.com>
2026-03-25 21:20:12 -0400

    add 238: Product of Array Except Self

--- solutions/0238-product-of-array-except-self.cpp
+++ solutions/0238-product-of-array-except-self.cpp
@@ -1,27 +1,26 @@
 // 238. Product of Array Except Self
 // Medium | Array
-// https://leetcode.com/problems/product-of-array-except-self
-// Time: O(n) | Space: O(1)
+// https://leetcode.com/problems/product-of-array-except-self/submissions/1959408341/
+// Time: O(n) | Space: O(n)
 
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> res (n, 1);
-
-        for (int i = 1; i < n; i++){
-            res[i] = res[i-1] * nums[i-1];
+        vector<int> results(nums.size(), 1);
+        // vector<int> suffix(nums.size(), 1);
+        for(int i = 1; i < nums.size(); i++){
+            results[i] = results[i-1] * nums[i-1];
         }
 
         int suffix = 1;
-        for (int i = n-1; i >= 0; i--){
-            res[i] *= suffix;
-            suffix *= nums[i];
+        for(int i = results.size()-1; i >= 0; i--){
+            results[i] = (results[i] * suffix);
+            suffix = nums[i] * suffix;
         }
-        return res;
+        return results;
     }
 };
 
-// [1, 2, 4, 6]
-// [1, 1, 2, 8]
-// [ 48,   24,  12,  8]
+/// [1, 2, 3, 4]
+/// [1, 1, 2, 6]
+// [ 24, 12, 4, 1]
