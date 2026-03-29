bc42bebfb1f9e0dcbb7e46a3e277347ece116b20
Trent Wantman <93285436+TrentWantman@users.noreply.github.com>
2026-03-29 11:55:58 -0400

    add 11: Container With Most Water

--- solutions/0011-container-with-most-water.cpp
+++ solutions/0011-container-with-most-water.cpp
@@ -1,18 +1,26 @@
 // 11. Container With Most Water
-// Medium | Two Pointers
-// https://leetcode.com/problems/container-with-most-water/
+// Medium | Array
+// https://leetcode.com/problems/container-with-most-water/submissions/1961250520/
 // Time: O(n) | Space: O(1)
 
 class Solution {
 public:
-    int maxArea(vector<int>& heights) {
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int min_height = 0;
         int max_area = 0;
-        int i = 0;
-        int j = heights.size()-1;
 
-        while(j>i){
-            max_area = max(max_area, (j-i) * min(heights[j], heights[i]));
-            heights[j] < heights[i] ? j-- : i++;
+        while (l < r){
+            min_height = min(height[l], height[r]);
+            max_area = max(max_area, (r-l) * min_height);
+            if(height[l] == height[r]){
+                l++;
+                r--;
+            }
+            else{
+                min_height == height[l] ? l++ : r--;
+            }
         }
         return max_area;
     }
