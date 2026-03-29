959261846bfc017b9b286e848048813991ed7e2f
Trent Wantman <93285436+TrentWantman@users.noreply.github.com>
2026-03-29 11:56:01 -0400

    add 98: Validate Binary Search Tree

---
+++ solutions/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,34 @@
+// 98. Validate Binary Search Tree
+// Medium | Tree
+// https://leetcode.com/problems/validate-binary-search-tree/
+// Time: O(1) | Space: O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool validate(TreeNode* root, long min, long max){
+        if(!root){
+            return true;
+        }
+
+        if(root->val > min && root->val < max){
+            return (validate(root->left, min, root->val) && validate(root->right, root->val, max));
+        }
+        return false;
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return validate(root, LONG_MIN, LONG_MAX);
+    }
+};
