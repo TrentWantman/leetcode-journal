1a8c46a7a1553cd4f8a080bb91631b53e04e80f2
Trent Wantman <93285436+TrentWantman@users.noreply.github.com>
2026-03-29 11:55:59 -0400

    add 15: 3Sum

--- solutions/0015-3sum.cpp
+++ solutions/0015-3sum.cpp
@@ -1,41 +1,44 @@
 // 15. 3Sum
-// Medium | Two Pointers
+// Medium | Array
 // https://leetcode.com/problems/3sum/
 // Time: O(n) | Space: O(n)
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> pairs;
-
+        
+        vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++){
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            vector<int> pair;
-            
-            int c = -1 * nums[i];
-            int a = i+1;
-            int b = nums.size() -1;
 
-            
-            while (b > a){
-                if (nums[a] + nums[b] == c){
-    pair = {nums[i], nums[a], nums[b]};
-    pairs.push_back(pair);
-    ++a;
-    --b;
-    while (a < b && nums[a] == nums[a-1]) a++;
-    while (a < b && nums[b] == nums[b+1]) b--;
-}
-                else if (nums[a] + nums[b] > c){
-                    --b;
+        for (int i = 0; i < nums.size(); i++){
+
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+
+            int l = i + 1;
+            int r = nums.size() -1;
+
+            while(l < r){
+                
+                int temp = nums[i] + nums[l] + nums[r];
+
+                if (temp == 0){
+                    
+                    vector<int> triplets = {nums[i], nums[l], nums[r]};
+                    res.push_back(triplets);
+
+                    while (l < r && nums[l] == nums[l+1]) l++; // skip duplicate l
+                    while (l < r && nums[r] == nums[r-1]) r--; // skip duplicate r
+                    l++;
+                    r--;
                 }
-                else if(nums[a] + nums[b] < c){
-                    ++a;
+                else if(temp > 0){
+                    r--;
+                }
+                else{
+                    l++;
                 }
             }
-
         }
-        return pairs;
+        return res;
     }
 };
